54867e045ad31809d1de6cab8e6f2e6c3cc064dc
Krushit Kaneriya <149571250+KrushtKaneriya@users.noreply.github.com>
2024-12-11 14:51:25 +0530

    Update indexExpress.js

--- server/indexExpress.js
+++ server/indexExpress.js
@@ -13,16 +13,28 @@ const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT;
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "website-with-slider.html"));
-})
-
-
 app.get('/krushit', () => {
     res.json({"message":"Hii This Is Krushit!!!"});
 })
+app.use((req,res,next)=>{
+    const secretcode = req.query.secret;
+    if(secretcode === '1234'){
+        req.isAuthorized = true;
+    }
+    else{
+        req.isAuthorized = false;
+    }
+    next();
+})
 
+app.get('/', (req, res) => {
+    if(req.isAuthorized){
+        res.sendFile(path.join(__dirname, "views", "website-with-slider.html"));
+}
+    else{
+        res.send("Unauthorized Access");
+    }
+})
 app.use(express.static(path.join(__dirname, "views")));
 
 
